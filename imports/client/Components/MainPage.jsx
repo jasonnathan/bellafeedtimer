1c8f7a0ec2e461870a1eb402038663119c0ce41f
Jason Nathan <jjnathanjr@gmail.com>
2016-09-19 21:40:07 +0800

    Fixed bug in click handler & enabled recording list

--- imports/client/Components/MainPage.jsx
+++ imports/client/Components/MainPage.jsx
@@ -1,75 +1,63 @@
 import React, { PropTypes, Component } from 'react';
-import { Page, Toolbar, Row, Col } from 'react-onsenui';
 import { connect } from 'react-redux';
+import { Page, Toolbar, Row, Col } from 'react-onsenui';
 import { startRecording, stopRecording } from '../Actions/currentSession.js';
 import RecordButton from './RecordButton.jsx';
+import RecordingList from './RecordingList.jsx';
 
 class MainPage extends Component {
   constructor( props ) {
     super( props );
     this.onRecordClick = this.onRecordClick.bind(this);
   }
-  onRecordClick(event){
-    let currentSession = this.props.currentSession,
-      position = event.currentTarget.id.replace( "record", "" ).toUpperCase(),
+
+  onRecordClick( event ){
+    const position = event.currentTarget.id.replace( "record", "" ).toUpperCase(),
       dispatch = this.props.dispatch;
 
-    currentSession = currentSession.recording
-      ? currentSession
-      : false;
+    let cs = this.props.currentSession || {};
 
-    if ( currentSession ) {
-      dispatch(stopRecording( currentSession._id ));
-
-      if ( currentSession.position === position )
-        return;
+    if(cs.recording){
+      dispatch(stopRecording( cs._id ));
     }
 
-    return dispatch(startRecording( position ));
+    return !cs.recording || cs.position !== position ? dispatch(startRecording( position )) : false;
   }
 
   isRecording( position ) {
     let cs = this.props.currentSession.recording;
-    return cs
-      ? cs.position === position
-      : false;
+    return cs ? cs.position === position : false;
   }
-  renderToolbar( ) {
+
+  get debugText() {
+    const {currentSession} = this.props;
+      return currentSession
+        ? JSON.stringify(currentSession, null, 2 ) + "\nNumber of sessions: "
+        : "";
+  }
+
+  renderToolbar() {
     return (
-      <Toolbar>
-        <div className="center">Bella Feed Timer</div>
-      </Toolbar>
+      <Toolbar> <div className="center">Bella Feed Timer</div> </Toolbar>
     );
   }
 
-  render( ) {
-    const {currentSession} = this.props,
-      text = currentSession
-        ? JSON.stringify(currentSession, null, 2 ) + "\nNumber of sessions: "
-        : "";
+  render() {
     return (
       <Page renderToolbar={this.renderToolbar}>
         <Row style={{ minHeight: '100px'}}>
           <Col verticalAlign="center" className="mainButtonContainer left">
-            <RecordButton
-              handler={this.onRecordClick}
-              position="LEFT"
-            />
+            <RecordButton handler={this.onRecordClick} position="LEFT" />
           </Col>
           <Col>&nbsp;</Col>
           <Col verticalAlign="center" className="mainButtonContainer right">
-            <RecordButton
-              handler={this.onRecordClick}
-              position="RIGHT"
-            />
+            <RecordButton handler={this.onRecordClick} position="RIGHT" />
           </Col>
         </Row>
         <Row>
           <Col>
-            {/* <RecordingList navigator={this.navigator} /> */}
-            <pre style={{padding: '1rem'}}>
-              <small>{text}</small>
-            </pre>
+            <RecordingList navigator={this.navigator} />
+            <pre style={{padding: '1rem'}}> <small>{this.debugText}</small></pre>
           </Col>
         </Row>
       </Page>
@@ -89,4 +77,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(MainPage)
+export default connect(mapStateToProps)(MainPage);
