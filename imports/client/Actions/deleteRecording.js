ca430340df8b294f62636f8df90311c718e000a2
Jason Nathan <jjnathanjr@gmail.com>
2016-09-19 21:38:22 +0800

    Removed deprecated action-creators

--- imports/client/Actions/deleteRecording.js
+++
@@ -1,9 +0,0 @@
-import {Meteor} from 'meteor/meteor';
-import {check} from 'meteor/check';
-
-export default function deleteRecording(id){
-  check(id, String);
-  return ()  => {
-    Meteor.call('DELETE_RECORDING', id);
-  }
-}
