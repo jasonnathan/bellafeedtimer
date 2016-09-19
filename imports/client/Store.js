7e92e246e189ad2ded334d4fe476e9576cb9db61
Jason Nathan <jjnathanjr@gmail.com>
2016-09-19 21:43:00 +0800

    Added thunk to middleware for async actions

--- imports/client/Store.js
+++ imports/client/Store.js
@@ -1,12 +1,12 @@
 import { applyMiddleware, createStore } from 'redux';
 import createLogger from 'redux-logger';
-// import thunk from 'redux-thunk';
+import thunk from 'redux-thunk';
 import persist from '/imports/client/Middleware/persist';
 import rootReducer from '/imports/client/Reducers/';
 
 // create a logger
 const logger = createLogger();
-const middleware = [logger, persist];
+const middleware = [thunk, logger, persist];
 
-const Store = createStore(rootReducer, {}, applyMiddleware(...middleware));
+const Store = createStore(rootReducer, {}, applyMiddleware(...middleware), window.devToolsExtension ? window.devToolsExtension() : f => f);
 export default Store;
