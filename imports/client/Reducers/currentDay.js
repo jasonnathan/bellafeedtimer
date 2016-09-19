77e9d37f05bd2737c38ccacd93e270658cd2cae6
Jason Nathan <jjnathanjr@gmail.com>
2016-09-19 21:42:20 +0800

    Created functionality for currentDay reducers

--- imports/client/Reducers/currentDay.js
+++ imports/client/Reducers/currentDay.js
@@ -1,6 +1,49 @@
 import moment from 'moment';
-export function currentDay(state = {
-  _id: moment().format('YYYYMMDD')
-}, action){
+import reject from 'lodash.reject';
+import map from 'lodash.map';
+import {
+  REQUESTED_TODAY,
+  RECEIVED_TODAY,
+  ERROR_FETCHING_TODAY,
+  UPDATE_TODAY
+} from '../Actions/currentDay';
+
+export default function currentDay(state = {
+  _id: moment()
+    .format('YYYYMMDD'),
+  sessions: [],
+  counts: {
+    duration: 0,
+    recordings: 0
+  },
+  isFetching: false,
+  isError: false,
+  err: null
+}, action) {
+  switch (action.type) {
+  case REQUESTED_TODAY:
+    return {...state,
+      isFetching: true,
+      isError: false,
+      err: null
+    };
+  case RECEIVED_TODAY:
+    return {...state,
+      ...action.currentDay,
+      isError: false,
+      isFetching: false,
+      err: null
+    }
+  case ERROR_FETCHING_TODAY:
+    return {...state,
+      isError: true,
+      err: action.err
+    }
+  case UPDATE_TODAY:
+    return {
+      ...state,
+      ...action.payload
+    }
+  }
   return state;
 }
