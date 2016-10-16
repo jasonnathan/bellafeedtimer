5618aa05313860f48b65c74d943b5706bab83c7f
Jason Nathan <jjnathanjr@gmail.com>
2016-10-16 14:06:50 +0800

    Finalised functionality for the list of recordings

--- imports/client/Components/RecordingList.jsx
+++ imports/client/Components/RecordingList.jsx
@@ -1,47 +1,56 @@
 import React, {PropTypes} from 'react';
 import Tracker from 'tracker-component';
 import {connect} from 'react-redux';
-import ons from 'onsenui';
 import {List} from 'react-onsenui';
+import moment from 'moment';
 import {TodayObserver} from '/imports/api/TodayObserver';
 import {fetchTodayAsync} from '../Actions/currentDay';
 import RecordingListHeader from './RecordingListHeader.jsx';
 import Recording from './Recording.jsx';
-// import RecordingDetailsPage from './RecordingDetailsPage.jsx';
-// import Recording from './Recording.jsx';
-//
+
 class RecordingList extends Tracker.Component {
   constructor(props) {
     super(props);
-    this._subs = [
-      this.subscribe("fetchToday"),
-      TodayObserver
-    ];
+    this._subs = [this.subscribe("fetchToday"), TodayObserver];
     // this.renderRecording = this.renderRecording.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.dispatch(fetchTodayAsync());
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this._subs.map(s => s && s.stop());
   }
 
-  viewRecording(row){
-    console.log(row);
+  renderRecording(row) {
+    return (<Recording key={row._id} session={row} />);
   }
 
-  renderRecording(row){
-    return (<Recording key={row._id} recording={row} />);
+  get dataSource() {
+    let sessions = this.props.sessions,
+      currentSession = this.props.currentSession,
+      found;
+    // if we don't have an on-going recording or we do and there are no other sessions
+    if (!currentSession.hasOwnProperty('started') || !sessions.length) {
+      return sessions;
+    }
+    // we have to look to see if the current recording is already in the sessions
+    // array
+    found = sessions.find(s => s._id === currentSession._id);
+    if (found) {
+      // replace the current session
+      sessions[sessions.indexOf(found)] = currentSession;
+    } else {
+      sessions = [ currentSession, ...sessions ];
+    }
+    return sessions;
   }
 
-  render(){
+  render() {
     return (
       <List
-        // length={10}
-        // calculateItemHeight={() => ons.platform.isAndroid() ? 60 : 60}
-        dataSource={this.props.sessions}
+        dataSource={this.dataSource}
         renderRow={this.renderRecording}
         renderHeader={() => (<RecordingListHeader />)}
       />
@@ -50,6 +59,7 @@ class RecordingList extends Tracker.Component {
 }
 
 RecordingList.propTypes = {
+  currentSession: PropTypes.object,
   currentDay: PropTypes.object,
   sessions: PropTypes.array,
   dispatch: PropTypes.func
@@ -59,47 +69,9 @@ const mapStateToProps = (state) => {
   let cd = state.currentDay;
   return {
     currentDay: cd,
+    currentSession: state.currentSession,
     sessions: cd.sessions || []
   };
 };
 
 export default connect(mapStateToProps)(RecordingList);
-
-//     const RecordingList = ({
-//         recordings = [],
-//         navigator
-//     }) => {
-//         const recordingClickHandler = index => {
-//             navigator.pushPage({
-//                 component: RecordingDetailsPage,
-//                 key: 'recording_DETAILS_PAGE',
-//                 recording: recordings[index],
-//                 props: {
-//                     recording: recordings[index]
-//                 }
-//             });
-//         };
-//
-//         const renderRecording = (recording, index) => {
-//             return (<Recording key={recording._id} onClick={() => recordingClickHandler(index)} recording={recording}/>);
-//         };
-//
-//         return (<List dataSource={recordings} renderRow={renderRecording} renderHeader={() => <ListHeader style={{
-//             color: '#444'
-//         }}>
-//             {incompleteCount}
-//         </ListHeader>}/>);
-//     };
-//
-//     RecordingList.propTypes = {
-//         recordings: PropTypes.array.isRequired,
-//         incompleteCount: PropTypes.number.isRequired,
-//         navigator: PropTypes.object
-//     };
-//
-//     let dtFormat = "[Today] - Do MMM (ddd)",
-//         currentTime = new ReactiveVar(moment().format(dtFormat))
-//     setInterval(() => {
-//         currentTime.set(moment().format(dtFormat))
-//     }, 1e3);
-// }
