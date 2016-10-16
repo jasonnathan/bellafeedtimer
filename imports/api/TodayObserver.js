68d9a0fd99ce1139d5798dba6745e6032f56c2d7
Jason Nathan <jjnathanjr@gmail.com>
2016-10-16 14:04:32 +0800

    changed formatting

--- imports/api/TodayObserver.js
+++ imports/api/TodayObserver.js
@@ -1,20 +1,12 @@
 import moment from 'moment';
+import {updateInToday} from '/imports/client/Actions/currentDay';
 import Store from '../client/Store';
-import {
-  updateInToday
-} from '/imports/client/Actions/currentDay';
-
-import {
-  Recordings
-} from './RecordingCollection';
+import {Recordings} from './RecordingCollection';
 
 const TodayObserver = Recordings
-  .find({
-    _id: moment()
-      .format('YYYYMMDD')
-  })
+  .find({_id: moment().format('YYYYMMDD')})
   .observeChanges({
-    changed: (id, fields) => Store.dispatch(updateInToday(fields)),
+    changed: (id: String, fields: Object) => Store.dispatch(updateInToday(fields)),
   })
 
 export default TodayObserver;
